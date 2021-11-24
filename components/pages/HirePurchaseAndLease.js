b18608fc71e827aa5fdafe014e2b02f4ea29cb7c
Akelly <akelly982@hotmail.com>
2021-11-24 20:38:20 +1100

    report to pdf now working for android and maybe IOS gotta check IOS

--- components/pages/HirePurchaseAndLease.js
+++ components/pages/HirePurchaseAndLease.js
@@ -2,8 +2,22 @@ import React from 'react';
 import {useState,useEffect} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, Animated} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
+import * as FileSystem from "expo-file-system";
+import * as Print from 'expo-print';
+import * as MediaLibrary from "expo-media-library";
+import * as Sharing from "expo-sharing";
+
+
 import {Spacer} from "../Spacer";
 // import { LeaseReport } from '../jsClass/LeaseReport';
+
+const htmlTestString =  '<!doctype html> <html lang="en"> <body> <div class="mainContainer"> <div class="reportContainer"> <div class="title"> <h3>Report Type:<h3> <h3>Hire Purchase</h3> </div> <div class="Client"> <div class="textAndVar"> <p>Client Name: </p> <p> jimmy </p> </div> <div class="spacer"> <p> ------------------------------- </p> </div> </div> <div class="headerData"> <div class="textAndVar"> <p>Principle: </p> <p> xxxxx </p> </div> <div class="textAndVar"> <p>Annual Interest rate: </p> <p> xxxxx </p> </div> <div class="textAndVar"> <p> rrr: </p> <p> xxxxx </p> </div> <div class="textAndVar"> <p>Principle: </p> <p> xxxxx </p> </div> <div class="textAndVar"> <p>Principle: </p> <p> xxxxx </p> </div> </div> <div class="spacer"> <p> ------------------------------- </p> </div> <div class="tableData"> <div class="row" id="rowName"> <p class="rowItem"> Increment </p> <p class="rowItem"> somthing else </p> <p class="rowItem"> somthing else </p> <p class="rowItem"> somthing else </p> <p class="rowItem"> somthing else </p> <p class="rowItem"> somthing else </p> <p class="rowItem"> somthing else </p> </div> <div class="row"> <p class="rowItem"> 1 </p> <p class="rowItem"> 1324432143 </p> <p class="rowItem"> 31241324132 </p> <p class="rowItem"> 100000.12 </p> <p class="rowItem"> 31423512341 </p> <p class="rowItem"> 123 </p> <p class="rowItem"> 9999 </p> </div> <div class="row"> <p class="rowItem"> 2 </p> <p class="rowItem"> 1324432143 </p> <p class="rowItem"> 3 </p> <p class="rowItem"> 100000.12 </p> <p class="rowItem"> 31423512341 </p> <p class="rowItem"> 12345678910 </p> <p class="rowItem"> 9999 </p> </div> <div class="row"> <p class="rowItem"> 3 </p> <p class="rowItem"> 1324432143 </p> <p class="rowItem"> 34132 </p> <p class="rowItem"> 100000.12 </p> <p class="rowItem"> 31423512341 </p> <p class="rowItem"> 12345678910 </p> <p class="rowItem"> 9999 </p> </div><div class="spacer"> <p> ---- ------------------------------------ </p> </div> <div class="tableDataInsert"> <div class="textAndVar"> <p> Year: </p> <p> 1 </p> </div> <div class="varLeft"> <p> 16000.65 </p> </div> <div class="varLeft"> <p> 11000.12 </p> </div> </div> <div class="spacer"> <p> ---- ------------------------------------ </p> </div> <div class="row"> <p class="rowItem"> 4 </p> <p class="rowItem"> 1324432143 </p> <p class="rowItem"> 31241324132 </p> <p class="rowItem"> 100000.12 </p> <p class="rowItem"> 31423512341 </p> <p class="rowItem"> 12345678910 </p> <p class="rowItem"> 9999 </p> </div> <div class="tableDataInsert"> <div class="itemCenter"> <p> Adjustment: </p> </div> <div class="itemLeft"> <p> 0.57 </p> </div> <div class="itemLeft"> <p> -0.57 </p> </div> </div> <div class="tableDataInsert"> <div class="itemCenter"> <p> Totals: </p> </div> <div class="itemLeft"> <p> 120000 </p> </div> <div class="itemLeft"> <p> 18000 </p> </div> </div> </div> </div> </div> </body> </html> <style> :root { --ReportColor: #fff; --ReportEdge: lightblue; --textColor: #3b3b3b; }p{ color: var(--textColor); } h3{ color: var(--textColor); } .mainContainer{ background-color: var(--ReportEdge); padding:25px; width: 925px; display: flex; justify-content: center; } .reportContainer{ background-color: var(--ReportColor); /* 875 px is the width of the grid it has fixed width partially because i want it to*/ /* be zoomable on the mobile devices */ width: 875px; padding: 20px; display: flex; flex-direction: column; }.title{ display: flex; flex-direction: row; justify-content: center; align-items: center; } .title h3{ padding-left: 5px; padding-right: 5px; } .spacer{ height: 30px; display: flex; justify-content: center; align-items: center; } .headerData{ width: 100%; display: grid; grid-template-columns: auto auto auto; } .textAndVar{ display: flex; flex-direction: row; align-items: center; justify-content: center; } .textAndVar p{ padding-left: 10px; padding-right: 10px; } /* ======== TABLE DATA ============ */ /* ================================ */ .tableData{ width: 100%; } .row{ display: grid; grid-template-columns: 125px 125px 125px 125px 125px 125px 125px; /* grid-template-columns: auto auto auto auto auto auto auto; */ } .rowItem{ display:flex; justify-content: center; align-items: center; } .tableDataInsert{ display: grid; grid-template-columns: 150px 125px 125px 125px 125px 125px 125px; } .tableEndText{ display: flex; flex-direction: column; justify-content: center; align-items: center; } .itemCenter{ display: flex; flex-direction: column; align-items: center; justify-content: center; } .itemLeft { display: flex; flex-direction: column; } </style>' 
+  
+
+
+
+
 
 
 // =======================================================
@@ -159,8 +173,8 @@ class Report {
         
         //Table Data ---------------------
 
-        // html components 
-            // standard row   <---- used for column names aswell not just data rows
+            // (1)   html row components <---- used for column names aswell not just data rows
+
         // <div class="row">
         //     <p class="rowItem"> 4 </p>
         //     <p class="rowItem"> 1324432143 </p>
@@ -171,7 +185,11 @@ class Report {
         //     <p class="rowItem"> 9999 </p>
         // </div>
 
-            //  year splitter
+
+
+
+            // (2) year splitter    <--- happens at annual increments
+
         // <div class="spacer">
         //     <p> ---- ----  ----  ----  ----  ----  ----  ----  ----  ---- </p>
         // </div>
@@ -191,7 +209,11 @@ class Report {
         //     <p> ---- ----  ----  ----  ----  ----  ----  ----  ----  ---- </p>
         // </div>
 
-            //end table data
+
+
+
+            // (3) end table data    <----- append to the end of the table does Adjustment ammount and final figures
+
         // <div class="tableDataInsert">
         //     <div class="itemCenter">
         //         <p> Adjustment: </p>
@@ -215,8 +237,10 @@ class Report {
         //     </div>
         // </div>
 
-        //TableData Code
 
+
+        //TableData Code
+            // Now to dynamically genereate our html code based on our template
 
         // log column names
         let strColumns = '<div class="row">'
@@ -323,6 +347,16 @@ class Report {
 
 
 }
+
+
+
+
+
+
+
+
+
+
 
 
 // ===================================================
@@ -566,6 +600,52 @@ export function HirePurchaseAndLease(props) {
 
     }
 
+    // ===========================================================
+    // ============= htmlToPdf ===================================
+
+    const createPDF = async (html) => {   // you can not change the html recieved var name
+        Print.Orientation = "portrait"
+        try {
+            const { uri } = await Print.printToFileAsync({ html });  //create a pdf file amd adds it to cache
+    
+            console.log("uri cache: " + uri)
+    
+            let f = FileSystem.documentDirectory + "ReportRight.pdf"
+            console.log("uri files: " + f)
+    
+            FileSystem.copyAsync({from:uri, to:f})
+    
+            if (Platform.OS === "ios") {
+              await Sharing.shareAsync(f);
+    
+            } else {   //android
+              const permission = await MediaLibrary.requestPermissionsAsync();
+              console.log(permission.status)
+              if (permission.granted){
+                await MediaLibrary.createAssetAsync(f)
+                .then((file) => {
+                  alert("Pdf downloaded check DCIM folder for reportRight.pdf")
+                })
+                .catch((err) =>{
+                    console.log(err)
+                })   
+              }
+            }
+    
+        } catch (err) {
+            console.error(err);
+        }
+      };
+    
+    
+      const htmlToPdf = () => {
+
+        //Print Libary
+        // createPDF(htmlTestString)
+        createPDF(props.reportStr)
+
+      }
+
 
 
     // ===========================================================
@@ -583,7 +663,7 @@ export function HirePurchaseAndLease(props) {
         if(report == null){
             alert("Please generate the report first")
         }else{
-            alert("download Report")
+            htmlToPdf()
         }
     }
 
